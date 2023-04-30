437f6a73ee5e6c847f4b697fd5fe5fafce551b04
Willian Raphael Faria Matos <willianr.mattos@gmail.com>
2023-04-30 18:13:41 -0400

    Adicionar projeto de relatorio e testes

---
+++ tests/FDC.Caixa.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FDC.Caixa.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/FDC.Seguranca.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FDC.Seguranca.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
